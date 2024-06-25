5a6f92077ec8ad362a47035876f448f6aa790917
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-06-25 12:25:06 +0530

    Exploring data types.

---
+++ data_types.java
@@ -0,0 +1,19 @@
+// exploring different data types in java
+
+public class data_types {
+    public static void main(String[] args) {
+        byte b = 8; 
+        System.out.println(b);
+
+        char ch = 'a'; // can store only 1 character value
+        System.out.println(ch);
+
+        boolean var = true; 
+        System.out.println(var);
+
+        double price = 10.5; // double can store larger values than float
+        System.out.println(price);
+
+
+    }
+}
