b09d84218273b7ad345fb3689aac3754a210d6b5
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-06-25 20:15:38 +0530

    Program to calculate sum of two numbers.

---
+++ sum_of_num.java
@@ -0,0 +1,11 @@
+// print sum of two numbers
+
+public class sum_of_num {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        System.out.println(sum);
+    }
+    
+}
