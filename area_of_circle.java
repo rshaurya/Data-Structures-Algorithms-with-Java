55b7fc3523c27fc9881d706c515f2ef694f376cd
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-06-25 20:15:17 +0530

    Program to calculate area of a circle.

---
+++ area_of_circle.java
@@ -0,0 +1,15 @@
+// calculate area of circle
+
+import java.util.*;
+
+public class area_of_circle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        float r = sc.nextFloat();
+        double area = 3.14f * r * r;
+        System.out.println(area);
+    }
+
+    
+}
