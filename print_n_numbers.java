91209ff10efac4df4057613fb2e7bf0004499006
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-06-29 22:13:01 +0530

    Program to print first n numbers.

---
+++ print_n_numbers.java
@@ -0,0 +1,21 @@
+// Program to print numbers till n.
+
+import java.util.Scanner;
+
+public class print_n_numbers {
+
+    public static void main(String[] args) {
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            int cnt = 1;
+
+            while (cnt <= n) {
+                System.out.println(cnt);
+                cnt++;
+            }
+        }
+        
+    }
+    
+}
