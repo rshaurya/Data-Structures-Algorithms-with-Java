41af323bd594528f57a118c8d178a944dd0ef687
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-07-01 22:15:24 +0530

    Program to print square pattern.

---
+++ sqaure_pattern.java
@@ -0,0 +1,20 @@
+// Program to make square pattern with n rows.
+
+import java.util.Scanner;
+
+public class sqaure_pattern {
+
+    public static void main(String[] args) {
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+
+            for (int i = 1; i <= n; i++) {
+                System.out.println("****");
+                
+            }
+        }
+
+    }
+    
+}
