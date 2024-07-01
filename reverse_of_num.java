08a584fc54017178e5177f9c475360d1936980d9
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-07-01 22:15:14 +0530

    Program to print reverse of a number.

---
+++ reverse_of_num.java
@@ -0,0 +1,25 @@
+// Program to print reverse of number.
+
+import java.util.Scanner;
+
+public class reverse_of_num {
+
+    public static void main(String[] args) {
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a number: ");
+
+            int num = sc.nextInt();
+            int rev_num = 0;
+
+            while (num > 0) {
+                rev_num = (rev_num * 10) + (num % 10);
+                num /= 10;
+            }
+            
+            System.out.println(rev_num);
+        }
+        
+    }
+    
+}
