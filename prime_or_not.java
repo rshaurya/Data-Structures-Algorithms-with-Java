ce3f6abc06c3681c10b7dc6c4ec8237f05af79d7
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-07-04 22:10:33 +0530

    Created function for isPrime.

--- prime_or_not.java
+++ prime_or_not.java
@@ -4,25 +4,28 @@ import java.util.*;;
 
 public class prime_or_not {
 
+    public static boolean isPrime(int n) {
+
+        if (n == 2) {
+            return true;
+        }
+
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            } 
+        }
+
+        return true;
+        
+    }
+
     public static void main(String[] args) {
         
         try (Scanner sc = new Scanner(System.in)) {
             int num = sc.nextInt();
 
-            if (num == 2) {
-                System.out.println(num + " is prime.");
-            }
-
-            for (int i = 2; i <= Math.sqrt(num); i++) {
-
-                if (num % i == 0) {
-                    System.out.println(num + " is not prime.");
-                    break;
-                } else {
-                    System.out.println(num + " is prime.");
-                }
-                
-            }
+            System.out.println(isPrime(num));
         }
     }
     
