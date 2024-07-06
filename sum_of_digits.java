d9f33a128476c29fa2f7253daff2c8fcbbdaacc0
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-07-06 22:43:14 +0530

    Program to compute sum of digits of a number.

---
+++ sum_of_digits.java
@@ -0,0 +1,32 @@
+// Program to compute sum of digits of a number.
+
+import java.util.Scanner;
+
+public class sum_of_digits {
+
+    public static int sumOfDigits(int n) {
+
+        int sum = 0;
+
+        while (n > 0) {
+            
+            int lastDigit = n % 10;
+            sum += lastDigit;
+
+            n /= 10;
+        }
+        
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter a number: ");
+            int n = sc.nextInt();
+
+            System.out.println("Sum of digts are: " + sumOfDigits(n));
+        }
+    }
+    
+}
