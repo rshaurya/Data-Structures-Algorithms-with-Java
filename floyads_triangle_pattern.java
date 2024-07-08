3d04da282411381e76c39992eff160f8efa444c4
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-07-08 21:04:49 +0530

    Program to print FLOYD'S Triangle.

---
+++ floyads_triangle_pattern.java
@@ -0,0 +1,31 @@
+// Program to print pattern of FLOYD'S triangle.
+
+/*
+ * 1
+ * 23
+ * 456
+ * 789
+ */
+
+public class floyads_triangle_pattern {
+
+    public static void floydsTriangle(int n) {
+        
+        int counter = 1;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(counter + " ");
+                counter++;
+            }
+
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        floydsTriangle(5);
+    }
+    
+}
