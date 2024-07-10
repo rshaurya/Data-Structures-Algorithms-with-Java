06adf813f0c15c1cdc292a2cec3d6c606d979a85
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-07-10 22:23:25 +0530

    Program to print hollow rhombus pattern.

---
+++ hollow_rhombus_pattern.java
@@ -0,0 +1,36 @@
+// Program to print hollow rhombus pattern.
+
+import java.util.Scanner;
+
+public class hollow_rhombus_pattern {
+
+    public static void hollowRhombus(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= n; j++) {
+                if (i == n || i ==1 || j == n || j == 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+            
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter number of rows: ");
+            int n = sc.nextInt();
+
+            hollowRhombus(n);
+        }
+    }
+    
+}
