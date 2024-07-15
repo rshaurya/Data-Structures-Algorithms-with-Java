dd21c7add18262799034a5d0b1d193e04449e3b3
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-07-15 21:36:21 +0530

    Updated code to find smallest number too.

--- largest_num_in_array.java
+++ largest_num_in_array.java
@@ -1,29 +1,33 @@
-// Program to find the largest number in an array.
+// Program to find the largest and the smallest number in an array.
 
 import java.util.*;
 
 
 public class largest_num_in_array {
 
-    public static int largestNum(int numbers[]) {
+    public static void largestAndSmallestNum(int numbers[]) {
         
         int largest = Integer.MIN_VALUE;
+        int smallest = Integer.MAX_VALUE;
 
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] > largest) {
                 largest = numbers[i];
             }
+            if (smallest > numbers[i]) {
+                smallest = numbers[i];
+            }
         }
         
-        return largest;
+        System.out.println("Largest value is: " + largest);
+        System.out.println("Smallest value is: " + smallest);
     }
 
     public static void main(String[] args) {
         
         int numbers[] = {2,8,6,9,7,12,25};
 
-        System.out.println("Largest number is: " + largestNum(numbers));
-
+        largestAndSmallestNum(numbers);
     }
     
 }
