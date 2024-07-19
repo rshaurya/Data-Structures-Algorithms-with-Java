ae2870749d8a32e71113528cc156ce5126343a1b
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-07-19 14:02:05 +0530

    Added code to print maximum and minimum sum of subarrays.

--- subarrays.java
+++ subarrays.java
@@ -5,11 +5,15 @@ public class subarrays {
 
     public static void printSubarrays(int numbers[]) {
         
+        int max_sum = 0;
         for (int i = 0; i < numbers.length; i++) {
             for (int j = i; j < numbers.length; j++) {
                 int sum = 0;
                 for (int k = i; k <= j; k++) {
                     sum += numbers[k];
+                    if (sum > max_sum) {
+                        max_sum = sum;
+                    } 
                     System.out.print(numbers[k] + " ");
                 }
                 System.out.print("Sum of the subarray is: " + sum);
@@ -17,6 +21,8 @@ public class subarrays {
             }
             System.out.println();
         }
+        System.out.println("Max sum is: " + max_sum);
+        System.out.println("Min sum is: " + numbers[0]);
 
     }
 
