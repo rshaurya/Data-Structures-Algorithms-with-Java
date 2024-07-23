fe17e20ea422e27d773962ae57e76fce39e90131
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-07-23 20:59:13 +0530

    Used Kadane's Algorithm to find maximum sum of a subarray.

--- subarrays.java
+++ subarrays.java
@@ -1,37 +1,30 @@
-// Program to print subarrays.
+// Program to print the maxinum sum of each subarray from a given array.
 
 
 public class subarrays {
 
     public static void maxSubarraySum(int numbers[]) {
+
+        // Using Kadane's Algorithm
         
         int max_sum = Integer.MIN_VALUE;
         int currentSum = 0;
-        int prefix[] = new int[numbers.length];
-
-        // Prefix array stores the sum upto i-th index of numbers array. Ex, prefix[2]
-        // will store the sum of elements (of numbers array) upto the 2nd index.
-
-        prefix[0] = numbers[0]; 
-        for (int i = 1; i < prefix.length; i++) {
-            prefix[i] = prefix[i-1] + numbers[i]; // creating every single element in the prefix array.
-        }
 
         for (int i = 0; i < numbers.length; i++) {
-            for (int j = i; j < numbers.length; j++) {
-                
-                currentSum = i == 0 ? prefix[j] : prefix[j] - prefix[i -1]; // calculating the sum
-                if (max_sum < currentSum) {
-                    max_sum = currentSum;
-                }
+            currentSum += numbers[i];
+            if (currentSum < 0) {
+                currentSum = 0; // if currentSum is negative, assign value as zero
             }
+
+            max_sum = Math.max(max_sum, currentSum);    
         }
+        
         System.out.println("Max sum is: " + max_sum);
 
     }
 
     public static void main(String[] args) {
-        int numbers[] = {2,-4,6,-1,3};
+        int numbers[] = {-2, -5, 8, 9};
 
         maxSubarraySum(numbers);
     }
