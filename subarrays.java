da11ee516b24b2f2d5b4f05d3a2743eee2474332
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-07-24 22:56:04 +0530

    Optimised the solution using Kadane's Algorithm.

--- subarrays.java
+++ subarrays.java
@@ -3,30 +3,35 @@
 
 public class subarrays {
 
-    public static void maxSubarraySum(int numbers[]) {
+    public static int maxSubarraySum(int numbers[]) {
 
         // Using Kadane's Algorithm
         
         int max_sum = Integer.MIN_VALUE;
         int currentSum = 0;
 
-        for (int i = 0; i < numbers.length; i++) {
-            currentSum += numbers[i];
-            if (currentSum < 0) {
-                currentSum = 0; // if currentSum is negative, assign value as zero
+        for (int i = 0; i < numbers.length; i++) { 
+            if (numbers[i] < 0) { // special case -  when all numbers in the array are negative.
+                max_sum = Math.max(max_sum, numbers[i]);
+            } else { 
+                currentSum += numbers[i];
+                if (currentSum < 0) { // if currentSum is negative, assign value as zero
+                    currentSum = 0;
             }
 
-            max_sum = Math.max(max_sum, currentSum);    
+            max_sum = Math.max(max_sum, currentSum);
+            }
         }
-        
-        System.out.println("Max sum is: " + max_sum);
+
+        System.out.print("Max sum is: ");
+        return max_sum;
 
     }
 
     public static void main(String[] args) {
-        int numbers[] = {-2, -5, 8, 9};
+        int numbers[] = {-2, -5};
 
-        maxSubarraySum(numbers);
+        System.out.println(maxSubarraySum(numbers));
     }
     
 }
