2696855649f6e4e453a939174ceedb9f0295ca3f
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-07-29 12:04:43 +0530

    Fixed logical error in code.

--- subarrays.java
+++ subarrays.java
@@ -11,25 +11,22 @@ public class subarrays {
         int currentSum = 0;
 
         for (int i = 0; i < numbers.length; i++) { 
-            if (numbers[i] < 0) { // special case -  when all numbers in the array are negative.
-                max_sum = Math.max(max_sum, numbers[i]);
-            } else { 
-                currentSum += numbers[i];
-                if (currentSum < 0) { // if currentSum is negative, assign value as zero
-                    currentSum = 0;
+            currentSum += numbers[i];
+            max_sum = Math.max(max_sum, currentSum);
+
+            if (currentSum < 0) { // if currentSum is negative, assign value as zero
+                currentSum = 0;
             }
 
-            max_sum = Math.max(max_sum, currentSum);
-            }
         }
 
-        System.out.print("Max sum is: ");
-        return max_sum;
-
+            return max_sum;
     }
 
+
+
     public static void main(String[] args) {
-        int numbers[] = {-2, -5};
+        int numbers[] = {4,-1,0,0,7};
 
         System.out.println(maxSubarraySum(numbers));
     }
