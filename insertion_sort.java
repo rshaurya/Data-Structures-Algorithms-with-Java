d5b1890193e3d7c70eb25b429e464d82fa87f436
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-08-11 10:13:53 +0530

    Program to implement Insertion Sort.

---
+++ insertion_sort.java
@@ -0,0 +1,23 @@
+// Program to implement Insertion Sort.
+
+public class insertion_sort {
+
+    public static void insertionSort(int arr[]) {
+        
+        for (int i = 0; i < arr.length-1; i++) {
+        }
+    }
+
+
+    public static void main(String[] args) {
+        
+        int arr[] = {5,4,1,3,2};
+
+        insertionSort(arr);
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i] + " ");
+        }
+    }
+    
+}
