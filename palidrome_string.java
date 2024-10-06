e2671988e4b72d5457170c6c98a22db7b027c6b6
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-10-06 12:41:41 +0530

    Program to check whether a string is a palindrome or not.

---
+++ palidrome_string.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+// Check if given string is a palindrome or not
+
+public class palidrome_string {
+
+    public static Boolean isPalindrome(String str) {
+        
+        for (int i = 0; i < str.length()/2; i++) {
+            if (str.charAt(i) != str.charAt(str.length()-i-1)) {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        String str;
+        str = sc.nextLine();
+
+        System.out.println(isPalindrome(str));
+
+    }
+    
+}
