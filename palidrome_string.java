86cb9f0da10892b927ab9c4928dbadf534225284
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-10-06 14:28:54 +0530

    Surrounded code with try block.

--- palidrome_string.java
+++ palidrome_string.java
@@ -18,11 +18,12 @@ public class palidrome_string {
 
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
-        String str;
-        str = sc.nextLine();
+        try (Scanner sc = new Scanner(System.in)) {
+            String str;
+            str = sc.nextLine();
 
-        System.out.println(isPalindrome(str));
+            System.out.println(isPalindrome(str));
+        }
 
     }
     
