daf617251d60fac4cb898f0b19b2b48ea278e6be
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-10-27 13:43:01 +0530

    Program to compress a given string.

---
+++ string_compression.java
@@ -0,0 +1,45 @@
+// Given a string 'aaabbcdd', return a compressed string like so,
+// 'a3b2c1d2'
+
+import java.util.Scanner;
+
+public class string_compression {
+
+    public static String compressString(String str) {
+        
+        
+        StringBuilder sb = new StringBuilder("");
+        
+        for (int i = 0; i < str.length(); i++) {
+
+            Integer count = 1;
+           
+            while (i<str.length()-1 && str.charAt(i) == str.charAt(i+1)) {
+                count++;
+                i++;
+            }
+            
+            sb.append(str.charAt(i));
+            if (count > 1) {
+                sb.append(count.toString());
+            }
+
+        }
+
+        return sb.toString();
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String str;
+        str = sc.nextLine();
+
+        System.out.println(compressString(str));
+        
+    }
+    
+}
