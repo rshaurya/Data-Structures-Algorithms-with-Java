5a61ae5239ce86a42eb4e90ca526c6d7f00236a8
rshaurya24 <shaurya.r.pethe@gmail.com>
2024-10-29 13:43:38 +0530

    Added comments.

--- is_anagram.java
+++ is_anagram.java
@@ -7,18 +7,22 @@ public class is_anagram {
 
     public static void isAnagram(String str1, String str2) {
         
+        // convert given strings to lower case
         str1 = str1.toLowerCase();
         str2 = str2.toLowerCase();
 
-        if (str1.length() == str2.length()) {
+        if (str1.length() == str2.length()) { // checking if length is the same
             
+            // converting string to character array
             char[] str1CharArray = str1.toCharArray();
             char[] str2CharArray = str2.toCharArray();
 
+            // sorting the array alphabetically
             Arrays.sort(str1CharArray);
             Arrays.sort(str2CharArray);
 
             boolean result = Arrays.equals(str1CharArray, str2CharArray);
+            
             if (result) {
                 System.out.println(str1 + " and " + str2 + " are anagrams of each other.");
             } else {
