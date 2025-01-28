1582617bef6cb89a1ddfbce492436ab16b7bbac0
rshaurya24 <shaurya.r.pethe@gmail.com>
2025-01-28 22:43:35 +0530

    Program to get ith bit of any given number.

---
+++ get_ith_bit.java
@@ -0,0 +1,23 @@
+// Program to get ith bit of any given number.
+
+public class get_ith_bit {
+
+    public static int GetIthBit(int n, int i) {
+
+        int bitMask = (1 << i);
+
+        if ((n & bitMask) == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+        
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(GetIthBit(10, 2));
+        
+    }
+    
+}
